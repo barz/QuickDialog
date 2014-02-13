c5d600328cc7104dac959a81e0c6a5e309418b2f
John C. Daub <hsoi@hsoi.com>
2014-02-13 10:36:49 -0600

    Adopt spaces instead of tabs to try to work around some spacing and layout issues. See comments.

--- extras/QPickerTabDelimitedStringParser.m
+++ extras/QPickerTabDelimitedStringParser.m
@@ -12,13 +12,29 @@
 
 - (id)objectFromComponentsValues:(NSArray *)componentsValues
 {
-    return [componentsValues componentsJoinedByString:@"\t"];
+    // Hsoi 2014-02-13 - tabs seem to cause weird spacing problems, especially with
+    // right-justified text and proportional-width fonts. Sometimes the "columns"
+    // wind up slammed against each other, sometimes they are spaced out.. it's all
+    // just the way things calculate out.
+    //
+    // We found using spaces at least makes things a little more deterministic. So
+    // we're going to use 2 spaces instead of a tab.
+    NSString* delimiter = @"  "; // @"\t";
+    return [componentsValues componentsJoinedByString:delimiter];
 }
 
 - (NSArray *)componentsValuesFromObject:(id)object
 {
+    // Hsoi 2014-02-13 - tabs seem to cause weird spacing problems, especially with
+    // right-justified text and proportional-width fonts. Sometimes the "columns"
+    // wind up slammed against each other, sometimes they are spaced out.. it's all
+    // just the way things calculate out.
+    //
+    // We found using spaces at least makes things a little more deterministic. So
+    // we're going to use 2 spaces instead of a tab.
+    NSString* delimiter = @"  "; // @"\t";
     NSString *stringValue = [object isKindOfClass:[NSString class]] ? object : [object description];
-    return [stringValue componentsSeparatedByString:@"\t"];
+    return [stringValue componentsSeparatedByString:delimiter];
 }
 
 @end
